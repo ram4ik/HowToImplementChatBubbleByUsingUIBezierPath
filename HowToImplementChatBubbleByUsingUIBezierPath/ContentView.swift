1e5c4e9e8c819a45f1dcac073d21ff550f12d995
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-03 14:30:58 +0200

    init

--- HowToImplementChatBubbleByUsingUIBezierPath/ContentView.swift
+++ HowToImplementChatBubbleByUsingUIBezierPath/ContentView.swift
@@ -9,8 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var msg = "Hi, \n I'm App-Designer, iOS apps developer and apps designer"
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text(msg)
+                .padding(12)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Bubble(chat: true))
+        }
+        .padding()
+        .background(Color.green)
+        .clipShape(Bubble(chat: true))
+    }
+}
+
+struct Bubble: Shape {
+    var chat: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topRight, .topLeft, chat ? .bottomLeft : .bottomRight], cornerRadii: CGSize(width: 20, height: 20))
+        
+        return Path(path.cgPath)
     }
 }
 
